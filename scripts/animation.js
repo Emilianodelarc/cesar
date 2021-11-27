b4ffe6a547d3548942b6e9d4f2dac8390003757b
Emilianodelarc <emilianodelarc@gmail.com>
2021-11-27 07:52:32 -0300

    arreglo de loading index

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,6 +1,15 @@
-$('#header, #main').hide()
-setTimeout(() => {
-    $('#imgLoading').fadeOut()
-    $('#header, #main').fadeIn(3000)
-}, 3000);
+
+if(sessionStorage.img){
+    $('#imgLoading').hide()
+}else{
+   $('#header, #main').hide()
+    setTimeout(() => {
+        $('#imgLoading').fadeOut()
+        $('#header, #main').fadeIn(3000)
+        sessionStorage.setItem('img', true)
+    }, 3000); 
+}
+
+
+
 
